410dd82a1136213887e5471cc7084193b5d76f83
Kraussie <nathanialkrauss@gmail.com>
2019-09-06 10:01:28 -0400

    Added HW comments

--- 00_In Class Activities/01_CmndLineGreet/01_CmndLineGreet.py
+++ 00_In Class Activities/01_CmndLineGreet/01_CmndLineGreet.py
@@ -1,3 +1,12 @@
+# Author: Nate K
+# Date of Creation: 09/06/2019
+# Date of Last Edit: 09/06/2019
+# This code has a short conversation with the user
+# Sources: None
+# On my honor, I have neither given nor received unauthorized aid.
+# Signed: NK 09/06/2019
+
+
 print('\n\n\nHello, World!')
 user_response = input('How are you doing today?\n\n\n')
 print("Oh, " +user_response+"?" " That's nice")
