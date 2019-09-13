f00b5299daadbeaaca8ffd4ebf08112bf04404c1
Kraussie <nathanialkrauss@gmail.com>
2019-09-13 13:03:33 -0400

    Init Commit of 1st Project Folder

---
+++ 00_Project Files/00_TextBasedGame/00_TextBasedGame.py
@@ -0,0 +1,9 @@
+# Author: Nate K
+# Date of Creation: 09/13/2019
+# Date of Last Edit: 09/13/2019
+# Text-Based Game Project
+# SOURCE: 
+
+# On my honor, I have neither given nor received unauthorized aid.
+# Signed: NK 09/26/2019
+# *+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*
